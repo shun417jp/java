5c1e3ef29e998d7db3321e0b93808bda9e391c6c
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2023-11-09 14:55:03 +0900

    1回の実行で２人分のBMIを出力

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import  java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         double height = Double.parseDouble(sc.nextLine());
         double weight = Double.parseDouble(sc.nextLine());
 
@@ -12,6 +13,16 @@ public class Main {
         DecimalFormat df = new DecimalFormat("#.##");
 
         System.out.println(df.format(bmi));
+
+
+        double height2 = Double.parseDouble(sc.nextLine());
+        double weight2 = Double.parseDouble(sc.nextLine());
+
+        double bmi2 = weight2/(height2*height2);
+
+        DecimalFormat DF = new DecimalFormat("#.##");
+
+        System.out.println(df.format(bmi2));
 
     }
 
