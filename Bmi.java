52c538667996f629c83a109d3f05fb945e791106
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2023-11-09 14:59:55 +0900

    Main.javaをBmi.javaへ変更

--- Bmi.java
+++ Bmi.java
@@ -1,7 +1,7 @@
 import java.text.DecimalFormat;
 import  java.util.Scanner;
 
-public class Main {
+public class Bmi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
